10c519a4863730ebc2fc53758bc98b1d2153d103
KINjoohyun <gurados@gmail.com>
2023-10-04 13:11:42 +0900

    SwipeManager  - 마우스 드래그 입력을 통한 LineRenderer 생성.  - MouseButtonUp 입력을 통해서 해당 Line을 검사하기 위한 기반이 마련.

---
+++ Assets/Scripts/SwipeManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeManager : MonoBehaviour
+{
+    private LineRenderer curLine;
+    private Camera cam;
+    private int positionCount = 2;
+    private Vector3 prevPos = Vector3.zero;
+
+    public float thick = 0.1f; // ¼±ÀÇ ±½±â
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        DrawingUpdate();
+    }
+
+    void DrawingUpdate()
+    {
+        Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartDrawing(mousePos);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            ConnectDrawing(mousePos);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            LineCheck();
+        }
+    }
+
+    void StartDrawing(Vector3 mousePos)
+    {
+        GameObject line = new GameObject("Line");
+        LineRenderer lineRend = line.AddComponent<LineRenderer>();
+
+        line.transform.parent = cam.transform;
+        line.transform.position = mousePos;
+
+        lineRend.startWidth = thick;
+        lineRend.endWidth = thick;
+        lineRend.startColor = Color.red;
+        lineRend.endColor = Color.red;
+        lineRend.numCornerVertices = 5;
+        lineRend.numCapVertices = 5;
+        lineRend.SetPosition(0, mousePos);
+        lineRend.SetPosition(1, mousePos);
+
+        curLine = lineRend;
+    }
+
+    void ConnectDrawing(Vector3 mousePos)
+    {
+        if (Vector3.Distance(prevPos, mousePos) >= 0.001f)
+        {
+            prevPos = mousePos;
+            positionCount++;
+            curLine.positionCount = positionCount;
+            curLine.SetPosition(positionCount - 1, mousePos);
+        }
+    }
+
+    void LineCheck()
+    {
+
+
+        DeleteLine();
+    }
+
+    void DeleteLine()
+    {
+        positionCount = 2;
+        prevPos = Vector3.zero;
+        Destroy(curLine.gameObject);
+        curLine = null;
+    }
+}
