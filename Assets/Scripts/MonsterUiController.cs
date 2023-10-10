3a67d801a262c4f70dd2ec431d2ccd779cfc1f29
KINjoohyun <gurados@gmail.com>
2023-10-10 18:12:21 +0900

    UIManager  - guideImage를 갱신하는 UpdateGuide 함수 추가.
    MonsterUiController
     - 코드 정리.
    
    MonsterSpawner
     - 몬스터 생성 시 UpdateGuide.
    
    GameManager
     - 가장 가까운 Monster의 Pattern을 반환하는 CloserPattern 함수 추가.
     - Monster가 리스트에서 삭제될 때 UpdateGuide.
    
    이미지 리소스 추가
     - guide류 이미지들 추가.

--- Assets/Scripts/MonsterUiController.cs
+++ Assets/Scripts/MonsterUiController.cs
@@ -9,7 +9,7 @@ public class MonsterUiController : MonoBehaviour
     public GameObject[] icons;
     public GameObject panel;
     private Queue<GameObject> queue = new Queue<GameObject>();
-    private GameObject curObj = null;
+    private GameObject inputObj = null;
     public ParticleSystem hitParticle;
 
     public void EnqueueImage(Pattern p)
@@ -17,25 +17,25 @@ public class MonsterUiController : MonoBehaviour
         switch (p)
         {
             case Pattern.Vertical:
-                curObj = Instantiate(icons[0]);
+                inputObj = Instantiate(icons[0]);
                 break;
             case Pattern.Horizontal:
-                curObj = Instantiate(icons[1]);
+                inputObj = Instantiate(icons[1]);
                 break;
             case Pattern.V:
-                curObj = Instantiate(icons[2]);
+                inputObj = Instantiate(icons[2]);
                 break;
             case Pattern.Caret:
-                curObj = Instantiate(icons[3]);
+                inputObj = Instantiate(icons[3]);
                 break;
             default:
-                curObj = null;
+                inputObj = null;
                 break;
         }
-        curObj.transform.SetParent(panel.transform, false);
-        if (curObj != null)
+        inputObj.transform.SetParent(panel.transform, false);
+        if (inputObj != null)
         { 
-            queue.Enqueue(curObj);
+            queue.Enqueue(inputObj);
         }
     }
 
