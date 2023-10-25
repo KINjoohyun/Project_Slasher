36329bc22b146c235837b6421f6ba298d724dc8e
KINjoohyun <gurados@gmail.com>
2023-10-25 16:21:28 +0900

    Stash Commit.

--- Assets/Scripts/InputTester.cs
+++ Assets/Scripts/InputTester.cs
@@ -3,25 +3,45 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class InputTester : MonoBehaviour
 {
+    private Camera maincam;
     public TextMeshProUGUI Text;
     private bool IsDraw = false;
 
     public GameObject stone;
 
+    private void Awake()
+    {
+        maincam = Camera.main;
+    }
+
     private void Update()
     {
         if (MultiTouchManager.instance.primary != int.MinValue)
         {
-            Text.text = Input.GetTouch(MultiTouchManager.instance.primary).position.ToString();
-            DrawLine(Input.GetTouch(MultiTouchManager.instance.primary).position);
+            var prime = Input.GetTouch(MultiTouchManager.instance.primary).position;
+            Vector3 pos = maincam.ScreenToWorldPoint(new Vector3(prime.x, prime.y, 1));
+
+            DrawLine(pos);
+        }
+        else
+        {
+            DeleteLine();
         }
     }
 
     private void DrawLine(Vector3 pos)
     {
+        stone.SetActive(true);
         stone.transform.position = pos;
+    }
+
+    private void DeleteLine()
+    {
+        stone.GetComponent<TrailRenderer>().Clear();
+        stone.SetActive(false);
     }
 }
