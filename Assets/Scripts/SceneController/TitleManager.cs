84372a93ac34ff88dd6079887210ad0f7d9c7d89
KINjoohyun <gurados@gmail.com>
2023-10-27 16:39:45 +0900

    Square Render Texture  - 입력 개선으로 인한 불필요로 삭제.
    TitleManager
     - Google Play 자동로그인 기능 추가. (test code)
    
    Monster_table.csv
     - 수치 조정.
    
    Upgrade_table.csv
     - 수치 조정.

--- Assets/Scripts/SceneController/TitleManager.cs
+++ Assets/Scripts/SceneController/TitleManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.Audio;
+using GooglePlayGames;
 
 public class TitleManager : MonoBehaviour
 {
@@ -18,11 +19,17 @@ public class TitleManager : MonoBehaviour
 
     private void Start()
     {
+        PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.Activate();
+        Social.localUser.Authenticate((bool success) => { });
+
         PlayDataManager.Init();
 
         mixer.SetFloat("masterVol", Mathf.Log10(PlayDataManager.data.masterVol) * 20);
         mixer.SetFloat("musicVol", Mathf.Log10(PlayDataManager.data.musicVol) * 20);
         mixer.SetFloat("sfxVol", Mathf.Log10(PlayDataManager.data.sfxVol) * 20);
+
+
     }
 
 }
